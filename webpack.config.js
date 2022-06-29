95fbf6d72f86b09ab3b5c30a5dafb8e3352a478d
Trinidad Padilla <tpadilla101@live.com>
2022-06-29 10:22:07 -0700

    finished

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 /* Have to use ES5 syntax for this */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     module: {
     /* Rules = way to tell webpack what it needs to do when it encounters certain file types: */
@@ -13,7 +15,24 @@ module.exports = {
                     /* We use Babel, a library that transpile JS and JSX into older versions of JS: */
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.css$/,
+            /* Loaders evaluate from right to left: */
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader'
+                }
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        /* Takes in a config file that is the template of the file used as an HTML starting point: */
+            template: './index.html'
+        })
+    ]
 }
